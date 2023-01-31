1db07e2ef49445c173ea15e3e13f97681fef24c7
Dmitry Sivokhin <dmitry.sivokhin@keenetic.net>
2023-01-31 16:20:16 +0300

    configuration file improvements

--- instance/config_demo.py
+++ instance/config_demo.py
@@ -1,18 +1,18 @@
-DEBUG_SKIP_CHECK_TIMESTAMP = True
-# DEBUG_SKIP_CALLBACK_BASICAUTH = True
+DEBUG_SKIP_CHECK_TIMESTAMP = False
+DEBUG_SKIP_CALLBACK_BASICAUTH = False
 
 RECORDSTORE = 'file'  # 'firestore' or 'file'
-RECORDSTORE_DIRPREFIX = '/tmp/link-daemon-temp'
+RECORDSTORE_DIRPREFIX = '/srv/cloud-link-service-python-example/data'
 
 FIRESTORE_PROJECT = 'your-gcp-project...'
 
-NDSS_SERVICE_ID = 'future-telecom'
-NDSS_SERVER = 'https://ndss'
+NDSS_SERVICE_ID = '<service-id received from Keenetic>'
+NDSS_SERVER = '<NDSS API URL received from Keenetic>'
 NDSS_CRT = '4096-KNT-root-ca'
-NDSS_TIMEOUT = 20
+NDSS_TIMEOUT = 30
 
-NDSS_CALLBACK_BASIC_LOGIN = '...'
-NDSS_CALLBACK_BASIC_PASSWORD = '...'
+NDSS_CALLBACK_BASIC_LOGIN = '<Frontend app login>'
+NDSS_CALLBACK_BASIC_PASSWORD = '<Frontend app password>'
 
-NDSS_AUTH_BASIC_LOGIN = '...'
-NDSS_AUTH_BASIC_PASSWORD = '...'
+NDSS_AUTH_BASIC_LOGIN = '<NDSS API login received from Keenetic>'
+NDSS_AUTH_BASIC_PASSWORD = '<NDSS API pass received from Keenetic'
